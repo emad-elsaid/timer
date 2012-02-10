2a573e1dc19d8e5c2eb77f15c09053b270e8c8a3
Emad Elsaid <blazeeboy@gmail.com>
2012-02-10 17:31:47 +0200

    added scrambling algorithm

--- timer.js
+++ timer.js
@@ -18,11 +18,12 @@
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *      MA 02110-1301, USA. 
  */
-var Timer = function(DOM_element){
+var Timer = function(DOM_element, DOM_scramble){
 	
 	this.start_t = null;
 	this.interval = null;
 	this.DOM = DOM_element;
+	this.DOM_scramble = DOM_scramble;
 	
 	/**
 	 * toggle start stop of current timer
@@ -50,15 +51,56 @@ var Timer = function(DOM_element){
 	}
 
 	/**
+	 * generate scrambling 
+	 */
+	 this.scramble = function(){
+		 
+		// predefine faces and other arrays
+		var moves = ['R','L','U','D','F','B'];
+		var prime = ['','`'];
+		var repeat = ['','2'];
+		var numberOfSteps = 25;
+
+		// let's scrumble
+		var scrambles = '';
+		var lastMove = -1;
+
+		// generate 25 moves
+		for( var i=0; i<numberOfSteps; i++ ){
+			
+			// make sure the next move is not the same as the last
+			do{
+				var thisMove = Math.floor(Math.random()*moves.length);
+			}while( thisMove==lastMove );
+			// set lastMove with thisMove
+			lastMove = thisMove;
+			
+			// will it be counter clockwise or clockwise
+			var thisPrime = Math.floor(Math.random()*prime.length);
+			
+			// will be repeated ? in case of it is not prime
+			var thisRepeat = thisPrime==0 
+				? Math.floor(Math.random()*repeat.length)
+				: 0;
+			
+			// add the new move to the scrambling array
+			scrambles += moves[thisMove]+prime[thisPrime]+repeat[thisRepeat]+' ';
+			
+		}
+
+		this.DOM_scramble.innerHTML = scrambles;
+	 }
+	 
+	/**
 	 * stop updating the DOM and clear the interval from object
 	 */
 	this.end = function(){
 		clearInterval(this.interval);
 		this.interval = null;
+		timer_obj.scramble();
 	}
 	
 	 this.update = function(){
-			
 			// get the ellapsed milliseconds
 			var ellapsed = ( (new Date()-this.start_t)/1000 );
 			// calculate minutes
@@ -69,6 +111,5 @@ var Timer = function(DOM_element){
 			// set minutes : seconds.milliseconds 
 			this.DOM.innerHTML = minutes.toString() + ':' + ellapsed;
 	}
-
 
 }
